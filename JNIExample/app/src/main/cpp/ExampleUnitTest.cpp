a2807e909e0e68398d9f0fd2a34dae178dd041ba
nomenas <naum.puroski@gmail.com>
2016-09-24 00:21:13 +0200

    init version JNIExample

---
+++ JNIExample/app/src/main/cpp/ExampleUnitTest.cpp
@@ -0,0 +1,17 @@
+#include <jni.h>
+#include <string>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+JNIEXPORT jint JNICALL
+Java_com_nomenas_jniexample_ExampleUnitTest_testMethod(JNIEnv *env, jobject instance) {
+
+    return 0;
+
+}
+
+#ifdef __cplusplus
+}
+#endif
