45cf8ca3fec3f2533f3c6fa109575df1c5dd7268
nomenas <naum.puroski@gmail.com>
2016-09-24 00:08:11 +0200

    initial commit

---
+++ AutoStartApplication/app/src/main/java/com/nomenas/autostartapplication/service.java
@@ -0,0 +1,34 @@
+package com.nomenas.autostartapplication;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
+
+
+public class service extends Service
+{
+    private static final String TAG = "MyService";
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+    public void onDestroy() {
+        Toast.makeText(this, "My Service Stopped", Toast.LENGTH_LONG).show();
+        Log.d(TAG, "onDestroy");
+    }
+
+    @Override
+    public void onStart(Intent intent, int startid)
+    {
+        Intent intents = new Intent(getBaseContext(),hello.class);
+        intents.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intents);
+        Toast.makeText(this, "My Service Started", Toast.LENGTH_LONG).show();
+        Log.d(TAG, "onStart");
+    }
+
+
+}
